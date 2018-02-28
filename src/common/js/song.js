4d30a20c5c532d79d0acea36a72b31d5b8a75d32
wkl--007 <499657357@qq.com>
2018-02-28 17:30:22 +0800

    替换音乐播放接口路径

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -41,7 +41,7 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    url: `http://thirdparty.gtimg.com/C100${musicData.songmid}.m4a?fromtag=38`
   })
 }
 
