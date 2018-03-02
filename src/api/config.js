28404374ccf99ebf9db859e16698b43884413b17
wkl--007 <499657357@qq.com>
2018-03-02 15:57:16 +0800

    解决热门歌单推荐数据getSongList接口错误的问题

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
 export const commonParams = {
-  g_tk: 5381,
+  g_tk: 1928093487,
   inCharset: 'utf-8',
   outCharset: 'utf-8',
   notice: 0,
@@ -7,14 +7,7 @@ export const commonParams = {
 }
 
 export const options = {
-  param: 'jsonpCallback',
-  prefix: 'tan'
+  param: 'jsonpCallback'
 }
 
-// 基于jsonp方库，设置自定义的cbname
-// 解决cbname未定义的问题
-export const options1 = {
-  param: 'jsonpCallback',
-  prefix: 'playlistinfoCallback'
-}
 export const ERR_OK = 0
