5fc896216cdf1a5fd06930284029984f935b4d2e
wkl--007 <499657357@qq.com>
2018-03-02 15:56:05 +0800

    解决热门歌单推荐数据getSongList接口错误的问题

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -37,7 +37,7 @@ export function getDiscList() {
 }
 
 export function getSongList(disstid) {
-  const url = 'https://c.y.qq.com/qzone/fcg-bin/fcg_ucc_getcdinfo_byids_cp.fcg'
+  const url = '/api/getSongList'
 
   const data = Object.assign({}, commonParams, {
     disstid,
@@ -50,5 +50,9 @@ export function getSongList(disstid) {
     needNewCode: 0
   })
 
-  return jsonp(url, data, options)
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
